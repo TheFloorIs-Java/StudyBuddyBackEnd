646c0095ce28f5b9d45ff5b0c9d95c34b147eb2d
Harveen G <harveen096@revature.net>
2022-09-28 13:37:45 -0700

    added subjects

---
+++ src/main/java/App/Repository/SubjectRepository.java
@@ -0,0 +1,14 @@
+package App.Repository;
+
+import App.Model.Subjects;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface SubjectRepository extends JpaRepository<Subjects, Integer> {
+
+    @Query("FROM Subjects WHERE subjectName= :subjectName")
+    public Subjects findBySubjectNameIs(@Param("subjectName")String subjectName);
+    @Query("FROM Subjects WHERE subjectId= :subjectId")
+    public Subjects findById(@Param("subjectId")int subjectId);
+}
