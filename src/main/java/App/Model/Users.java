54e1944844471e0d932da49c5a4479a209dc299f
Harveen G <harveen096@revature.net>
2022-11-01 10:35:04 -0700

    changed to aws postgres

--- src/main/java/App/Model/Users.java
+++ src/main/java/App/Model/Users.java
@@ -3,9 +3,11 @@ package App.Model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -23,6 +25,7 @@ public class Users {
     public String username;
     @Column
     public String password;
+
 
 
 }
